68ad94dce2f8ec44d7d2607ca981feed2751d2de
Yuriy <liparg104@gmail.com>
2024-08-06 21:31:40 +0300

    [FIX] fixed local session issue

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,11 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ScheduleModule } from '@nestjs/schedule';
 import { TelegrafModule } from 'nestjs-telegraf';
 
-import * as LocalSession from 'telegraf-session-local';
 import { BotModule } from './bot/bot.module';
 import { RecordModule } from './record/record.module';
 import { UserModule } from './user/user.module';
+
+const LocalSession = require('telegraf-session-local');
 
 const session = new LocalSession({ database: './tmp/local_session.json' });
 
